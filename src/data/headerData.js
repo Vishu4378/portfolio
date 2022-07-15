80045d463187f4ba287d5b861b9decdf78157f72
Vishu4378 <98205038+Vishu4378@users.noreply.github.com>
2022-07-15 12:33:18 +0530

    first commit

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,7 @@ export const headerData = {
     name: 'Vijay Pandey',
     title: "Welcome to my world",
     desciption: "If there’s one thing you learn by working on a lot of different Web sites, it’s that almost any design idea–no matter how appallingly bad–can be made usable in the right circumstances, with enough effort.",
-    image: 'https://cdn.discordapp.com/attachments/938081983328817204/993540803580407900/DSC04991.JPG',
+    image: 'https://drive.google.com/uc?export=view&id=1RmYUrfbtgYfOc67ztnUJr37bK1PeZMkP',
     resumePdf: resume
 }
+// https://drive.google.com/file/d/1RmYUrfbtgYfOc67ztnUJr37bK1PeZMkP/view?usp=sharing
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,76 +11,76 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'News App',
+        projectDesc: 'This project shows current news ',
+        tags: ['React', 'CSS' ],
+        code: 'https://github.com/Vishu4378/News-app',
+        demo: 'https://vishu4378.github.io/News-app/',
         image: one
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Work with Text',
+        projectDesc: 'In This project we can perform some task like uppercase , copy text ,No of word ,Dark mode etc.',
+        tags: ['React'],
+        code: 'https://github.com/Vishu4378/Text-master',
+        demo: 'https://vishu4378.github.io/Text-master/',
         image: two
     },
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Facebook login page clone',
+        projectDesc: 'In this project i have clone fb login page design.',
+        tags: ['HTML','Tailwind CSS'],
+        code: 'https://github.com/Vishu4378/fb-login-clone-tailwind',
+        demo: 'https://github.com/Vishu4378/fb-login-clone-tailwind',
         image: three
     },
     {
         id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Microsoft Home clone',
+        projectDesc: 'In this project i have clone Microsoft Home page design.',
+        tags: ['HTML','Tailwind Css'],
+        code: 'https://github.com/Vishu4378/Microsoft-Clone-tailwind-',
+        demo: 'https://vishu4378.github.io/Microsoft-Clone-tailwind-/',
         image: four
     },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
+    // {
+    //     id: 5,
+    //     projectName: 'E-Commerce App',
+    //     projectDesc: 'A Simple E-commerce application',
+    //     tags: ['React Native', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: five
+    // },
+    // {
+    //     id: 6,
+    //     projectName: 'Uber Lite',
+    //     projectDesc: 'Uber clone',
+    //     tags: ['Flutter'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: six
+    // },
+    // {
+    //     id: 7,
+    //     projectName: 'Stock Market App',
+    //     projectDesc: 'A simple stock market API app',
+    //     tags: ['React', 'Redux', 'Bootstrap'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: seven
+    // },
+    // {
+    //     id: 8,
+    //     projectName: 'Car Pooling System',
+    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+    //     tags: ['Flutter', 'React'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: eight
+    // },
 ]
 
 
